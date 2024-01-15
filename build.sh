588d25ab7e335c436000b9068a8737cef3d7570c
Lars Nieuwenhuizen <l.nieuwenhuizen@fullstaq.com>
2024-01-15 19:32:11 +0000

    ci: Add PR build flow

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -eux;
 
-tag="latest";
+if [ $# -eq 1 ]; then
+    tag=$1;
+else
+  tag="latest";
+fi
 
 export DOCKER_BUILDKIT=1
 
-docker buildx build --no-cache --platform linux/amd64,linux/arm64 --target=fpm -t larsnieuwenhuizen/technight-php:latest --push .
-docker buildx build --no-cache --platform linux/amd64,linux/arm64 --target=webserver -t larsnieuwenhuizen/technight-webserver:latest --push .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 --target=fpm -t larsnieuwenhuizen/technight-php:${tag} --push .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 --target=webserver -t larsnieuwenhuizen/technight-webserver:${tag} --push .
